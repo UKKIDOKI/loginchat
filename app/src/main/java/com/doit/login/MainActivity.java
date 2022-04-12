727e6d34de342eeaca86e8133535d99560b68d09
김재욱 <ukkidoki@gimjaeug-ui-macbookpro.local>
2022-04-12 11:32:36 +0900

    Initial commit

---
+++ app/src/main/java/com/doit/login/MainActivity.java
@@ -0,0 +1,43 @@
+package com.doit.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+ private FirebaseAuth mFirebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mFirebaseAuth.signOut();
+                finish();
+            }
+        });
+
+         mFirebaseAuth.getCurrentUser().delete();
+    }
+}
