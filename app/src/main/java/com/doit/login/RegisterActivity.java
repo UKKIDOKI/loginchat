f580a306a1d8271436b5619e34c56977c5c0dc4a
ukkidoki <homerun1032@gmail.com>
2022-04-20 10:56:01 +0900

    로그인 입력 로직추가

--- app/src/main/java/com/doit/login/RegisterActivity.java
+++ app/src/main/java/com/doit/login/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.doit.login;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -45,7 +46,9 @@ public class RegisterActivity extends AppCompatActivity {
                 String strEmail = mEtEmail.getText().toString();
                 String strPwd = mEtpwd.getText().toString();
 
-
+                if (strEmail.equals(null) || strPwd.equals(null)) {
+                    Toast.makeText(RegisterActivity.this, "값을 입력해주세요", Toast.LENGTH_SHORT).show();
+                }
                 // FirebaseAyth 진행
                 mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
@@ -61,6 +64,7 @@ public class RegisterActivity extends AppCompatActivity {
                             // setvalue 데이터베이스에 입력
                             mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
                             Toast.makeText(RegisterActivity.this, "회원가입에 성공 하셨습니다 .", Toast.LENGTH_SHORT).show();
+                            finish();
                         } else {
                             Toast.makeText(RegisterActivity.this, "회원가입에 실패 하셨습니다 .", Toast.LENGTH_SHORT).show();
 
